6a17075e0f53130640e67270c666e9fbbefa587a
Roxas1197 <keyblader97@msn.com>
2019-12-10 19:56:04 -0600

    Update installingcooja.sh

--- installingcooja.sh
+++ installingcooja.sh
@@ -4,10 +4,17 @@ apt-get install apt-transport-https ca-certificates curl gnupg-agent software-pr
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update && apt-get install docker-ce docker-ce-cli containerd.io git -y
-usermod -aG docker `whoami`
 docker pull contiker/contiki-ng
+groupadd docker
+usermod -aG docker `whoami`
 git clone https://github.com/contiki-ng/contiki-ng.git
 cd contiki-ng
 git submodule update --init --recursive
-export CNG_PATH=`pwd`/contiki-ng
-alias contiker="docker run --privileged --sysctl net.ipv6.conf.all.disable_ipv6=0 --mount type=bind,source=$CNG_PATH,destination=/home/user/contiki-ng -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp.X11-unix -v /dev/bus/usb -ti contiker/contiki-ng"
+
+#####YOU HAVE TO RUN
+
+###export CNG_PATH=/home/<user>/contiki-ng/
+
+###alias contiker="docker run --privileged --sysctl net.ipv6.conf.all.disable_ipv6=0 --mount type=bind,source=$CNG_PATH,destination=/home/user/contiki-ng -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/bus/usb:/dev/bus/usb -ti contiker/contiki-ng"
+
+###usermod -aG docker <user>
